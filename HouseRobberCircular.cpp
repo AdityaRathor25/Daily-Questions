542a5aa6a014998e677d59e075b23f969001ee36
Aditya <aditya.rathor.2505@gmail.com>
2023-04-15 13:36:38 +0530

    added description

--- HouseRobberCircular.cpp
+++ HouseRobberCircular.cpp
@@ -1,35 +1,82 @@
-#include<iostream>
-#include<bits/stdc++.h>
+/*You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+Example 2:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 3
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 1000
+*/
+
+
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int solve(vector<int>& nums , int ind , int lastInd)
+int rob(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+        return nums[0];
+    int startInd = 0;
+    int secondInd = 1;
+    int lastInd = n - 1;
+    int secondLastInd = n - 2;
+    int excludeLast = solve(nums, startInd, secondLastInd);
+    int excludeFirst = solve(nums, secondInd, lastInd);
+    return max(excludeLast, excludeFirst);
+}
+long long solve(vector<int> &nums, int stInd, int n)
+{
+    long long prev2 = 0;
+    long long prev1 = nums[stInd];
+    for (int i = stInd + 1; i <= n; i++)
     {
-        int prev2=0;
-        int prev1 = nums[ind];
-        for(int i = ind+1 ; i<= lastInd ; i++ )
-        {
-            int include = prev2 + nums[i];
-            int exclude = prev1;
-            int curr = max ( include , exclude );
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+        long long ans = max(nums[i] + prev2, prev1);
+        prev2 = prev1;
+        prev1 = ans;
     }
-int rob(vector<int>& nums) {
-        int n= nums.size();
-        if(n==1)
-            return nums[0];
-        int startInd=0;
-        int secondInd = 1;
-        int lastInd = n-1;
-        int secondLastInd = n-2;
-        int excludeLast = solve(nums ,startInd , secondLastInd);
-        int excludeFirst = solve(nums , secondInd , lastInd );
-        return max(excludeLast , excludeFirst);
+    return prev1;
+}
+
+long long int houseRobber(vector<int> &valueInHouse)
+{
+    int n = valueInHouse.size();
+    if (n == 1)
+    {
+        return valueInHouse[0];
     }
+
+    // case 1 he robbs 1st house and excludes last
+    // 0-based indexing
+    long long sum1 = solve(valueInHouse, 0, n - 2);
+
+    // case2 he excludes 1st and robbs last
+    // 0-based indexing
+    long long sum2 = solve(valueInHouse, 1, n - 1);
+    return max(sum1, sum2);
+}
 int main()
 {
-    vector<int> nums = {1,2,3,1};
-    cout<<rob(nums);
+    vector<int> nums = {1, 2, 3, 1};
+    cout << rob(nums);
     return 0;
 }
--- coinChange.cpp
+++ coinChange.cpp
@@ -1,3 +1,26 @@
+/*You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+*/
+
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std ;
@@ -24,8 +47,8 @@ int solve ( vector<int>& coins , int amount  )
     }
 int main()
 {
-    vector<int> fruits = {1,2,3};
-    int n = coinChange(fruits, 11);
+    vector<int> coins = {1,2,5};
+    int n = coinChange(coins, 11);
     cout<< "Ans = "<< n;
     return 0;
 }
